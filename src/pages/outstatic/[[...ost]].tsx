7a5ff8160a9a78dea68073d123bdfe4099432175
Lukman <abdulrauflukman9@gmail.com>
2024-07-09 13:47:51 +0100

    outstatic

---
+++ src/pages/outstatic/[[...ost]].tsx
@@ -0,0 +1,7 @@
+import "outstatic/outstatic.css";
+import { Outstatic } from "outstatic";
+import { OstClient } from "outstatic/client";
+
+export default Outstatic;
+
+// export const getServerSideProps = OstSSP;
