5bd2afc706ad18966b120e9c258e4d233febe33e
AOMuiz <abdulwasiumuiz@gmail.com>
2024-07-10 12:06:31 +0100

    feat: configure outstatic

--- src/pages/api/outstatic/[[...ost]].tsx
+++ src/pages/api/outstatic/[[...ost]].tsx
@@ -1,3 +1,9 @@
 import { OutstaticApi } from "outstatic";
 
 export default OutstaticApi;
+
+// import { OutstaticApi } from "outstatic";
+
+// export const GET = OutstaticApi.GET;
+
+// export const POST = OutstaticApi.POST;
--- src/pages/outstatic/[[...ost]].tsx
+++ src/pages/outstatic/[[...ost]].tsx
@@ -1,7 +1,33 @@
 import "outstatic/outstatic.css";
-import { Outstatic } from "outstatic";
-import { OstClient } from "outstatic/client";
+import { Outstatic, OstSSP } from "outstatic";
 
 export default Outstatic;
 
-// export const getServerSideProps = OstSSP;
+export const getServerSideProps = OstSSP;
+
+// import "outstatic/outstatic.css";
+// import { Outstatic, OutstaticData } from "outstatic";
+// import { OstClient } from "outstatic/client";
+
+// export default async function Page({
+//   ostData,
+//   params,
+// }: {
+//   ostData: OutstaticData;
+//   params: { ost: string[] };
+// }) {
+//   return <OstClient ostData={ostData} params={params} />;
+// }
+
+// // Fetch data server-side
+// export async function getServerSideProps(context) {
+//   const { params } = context;
+//   const ostData = await Outstatic();
+
+//   return {
+//     props: {
+//       ostData,
+//       params,
+//     },
+//   };
+// }
