f47d258e709b93ac831d7bfb76a4dd4f0b1b1aac
Lukman10a <abdulrauflukman9@gmail.com>
2024-08-05 16:26:02 +0100

    google analytic now working

--- src/components/googleAnalytics.tsx
+++ src/components/googleAnalytics.tsx
@@ -5,17 +5,17 @@ const GoogleAnalytics = () => {
   return (
     <>
       <Script
-        strategy="lazyOnload"
+        async
         src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_TRACKING_ID}`}
       />
 
-      <Script id="" strategy="lazyOnload">
+      <Script id="google-analytics">
         {`
               window.dataLayer = window.dataLayer || [];
               function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());             
-              gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_TRACKING_ID}', {
-              page_path: window.location.pathname,
+              gtag('js', new Date());  
+
+              gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_TRACKING_ID}', {             
               });
           `}
       </Script>
